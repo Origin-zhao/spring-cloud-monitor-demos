7687322c458f2a8d3182de00641b4385488bfbe5
joyuan <enginezy@gmail.com>
2018-03-11 18:11:14 +0800

    init

---
+++ a-server/src/main/java/com/joyuan/web/IndexController.java
@@ -0,0 +1,26 @@
+package com.joyuan.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index1")
+    public String index1(){
+        return "index1";
+    }
+
+    @RequestMapping("/index2")
+    public String index2(){
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "index2";
+    }
+
+}
