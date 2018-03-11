57b8a193dbd4a93f752008e561e019907c18f35f
joyuan <enginezy@gmail.com>
2018-03-11 17:05:28 +0800

    init

---
+++ a-server/src/main/java/com/joyuan/AServerApplication.java
@@ -0,0 +1,12 @@
+package com.joyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AServerApplication.class, args);
+	}
+}
